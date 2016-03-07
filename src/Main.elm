16b7d4f8fed78dc3576892bc95d1f9f7e8b29a18
Flux Xu <fluxxu@gmail.com>
2016-03-07 14:20:42 -0500

    add elm-hot-loader

--- src/Main.elm
+++ src/Main.elm
@@ -1,27 +1,34 @@
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing ( onClick )
-import String
+import Effects exposing (Effects)
 
 -- official 'Elm Architecture' package
 -- https://github.com/evancz/start-app
-import StartApp.Simple as StartApp
+import StartApp
 
 -- component import example
 import Components.Hello exposing ( hello )
 
 -- APP KICK OFF!
-main =
-  StartApp.start
-    { model = model
-    , view = view
-    , update = update
-    }
+app = StartApp.start
+  { init = init
+  , update = update
+  , view = view
+  , inputs = [ Signal.map (always NoOp) swap]
+  }
 
+main = app.html
+
+-- HOT-SWAPPING
+port swap : Signal.Signal Bool
 
 -- MODEL
-model = 0
+type alias Model = Int
 
+-- INIT
+init =
+  (0, Effects.none)
 
 -- VIEW
 -- Examples of:
@@ -39,11 +46,14 @@ view address model =
 
 
 -- UPDATE
-type Action = Increment
+type Action 
+  = NoOp
+  | Increment
 
 update action model =
   case action of
-    Increment -> model + 1
+    NoOp -> ( model, Effects.none )
+    Increment -> ( model + 1, Effects.none )
 
 
 -- CSS STYLES
