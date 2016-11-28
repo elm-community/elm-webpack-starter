ff24fe09ddf25a6915c30522aa4f12505199419a
yamamoto <hiroki.yamamoto@enigmo.co.jp>
2016-11-28 18:37:34 +0900

    Fix wrong comment

--- src/static/index.js
+++ src/static/index.js
@@ -1,6 +1,6 @@
 // pull in desired CSS/SASS files
 require( './styles/main.scss' );
-var $ = jQuery = require( '../../node_modules/jquery/dist/jquery.js' );           // <--- remove if Bootstrap's JS not needed
+var $ = jQuery = require( '../../node_modules/jquery/dist/jquery.js' );           // <--- remove if jQuery not needed
 require( '../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.js' );   // <--- remove if Bootstrap's JS not needed 
 
 // inject bundled Elm app into div#main
