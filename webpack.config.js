176f221104bc58417bc573de50fce8e4c67961a3
Peter Morawiec <petermorawiec@gmail.com>
2017-01-12 09:03:27 -0800

    update packages, wacky path fix when building for prod

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ var commonConfig = {
   output: {
     path:       outputPath,
     filename:   path.join( 'static/js/', outputFilename ),
-    publicPath: '/'
+    // publicPath: '/'
   },
 
   resolve: {
