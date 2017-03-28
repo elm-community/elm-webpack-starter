e1195b5374b06cc05ce9b8abc2de1cbcc4ad1a37
Joshua Byler <bylerj@gmail.com>
2017-03-28 15:21:45 -0400

    updated main. now it imports all of the other modules

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,7 +1,10 @@
 module Main exposing (..)
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing ( onClick )
+
+import Html exposing (beginnerProgram)
+import Models exposing (Model, initialModel)
+import Msgs exposing (Msg)
+import Update exposing (update)
+import View exposing (view)
 
 -- component import example
 import Components.Hello exposing ( hello )
@@ -10,54 +13,4 @@ import Components.Hello exposing ( hello )
 -- APP
 main : Program Never Int Msg
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
-
-
--- MODEL
-type alias Model = Int
-
-model : number
-model = 0
-
-
--- UPDATE
-type Msg = NoOp | Increment
-
-update : Msg -> Model -> Model
-update msg model =
-  case msg of
-    NoOp -> model
-    Increment -> model + 1
-
-
--- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
-view : Model -> Html Msg
-view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          img [ src "static/img/elm.jpg", style styles.img ] []                             -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
-        ]
-      ]
-    ]
-  ]
-
-
--- CSS STYLES
-styles : { img : List ( String, String ) }
-styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
+  Html.beginnerProgram { model = initialModel, view = view, update = update }
