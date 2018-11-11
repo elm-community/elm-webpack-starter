e8cbf06ad84e50e75d21e5ae47b30201d9f9efe5
Dan Murphy <d.p.murph19@gmail.com>
2018-11-10 20:46:34 -0700

    Added support for newer elm version and upgraded packages
    Added support for elm 0.19
    Updated dependencies

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,4 +1,5 @@
 module Main exposing (..)
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing ( onClick )
@@ -8,9 +9,9 @@ import Components.Hello exposing ( hello )
 
 
 -- APP
-main : Program Never Model Msg
+main : Program () Model Msg
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+  Browser.sandbox { init = model, view = view, update = update }
 
 
 -- MODEL
@@ -24,23 +25,23 @@ model = 0
 type Msg = NoOp | Increment
 
 update : Msg -> Model -> Model
-update msg model =
+update msg m =
   case msg of
-    NoOp -> model
-    Increment -> model + 1
+    NoOp -> m
+    Increment -> m + 1
 
 
 -- VIEW
 -- Html is defined as: elem [ attribs ][ children ]
 -- CSS can be applied via class names or inline style attrib
 view : Model -> Html Msg
-view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
+view m =
+  div [ class "container", style "margin-top" "30px", style "text-align" "center" ][    -- inline CSS (literal)
     div [ class "row" ][
       div [ class "col-xs-12" ][
         div [ class "jumbotron" ][
-          img [ src "static/img/elm.jpg", style styles.img ] []                             -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
+          img [ src "static/img/elm.jpg", style "width" "33%", style "border" "4px solid #337AB7" ] []
+          , hello m                                                                     -- ext 'hello' component (takes 'model' as arg)
           , p [] [ text ( "Elm Webpack Starter" ) ]
           , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
             span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
@@ -50,14 +51,3 @@ view model =
       ]
     ]
   ]
-
-
--- CSS STYLES
-styles : { img : List ( String, String ) }
-styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
--- src/static/index.js
+++ src/static/index.js
@@ -5,4 +5,4 @@ require( '../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.js' ); 
 
 // inject bundled Elm app into div#main
 var Elm = require( '../elm/Main' );
-Elm.Main.embed( document.getElementById( 'main' ) );
+Elm.Elm.Main.init( {node: document.getElementById( 'main' )} );
